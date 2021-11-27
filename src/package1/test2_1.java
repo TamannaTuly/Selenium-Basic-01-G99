4c45e9b78f0b1c65c142e225ee00de8ba54f63fd
TamannaTuly <ttniger53@gmail.com>
2021-11-28 01:47:59 +0600

    Test03-switch between frames

--- src/package1/test2_1.java
+++ src/package1/test2_1.java
@@ -1,5 +1,6 @@
 package package1;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,7 +9,11 @@ public class test2_1 {
         System.setProperty("webdriver.chrome.driver", "E:\\doctor strange\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
+        String baseUrl="http://demo.guru99.com/selenium/deprecated.html";
 
-
+        driver.get(baseUrl);
+        driver.switchTo().frame("classFrame");
+        driver.findElement(By.xpath("/html/body/div[4]/ul/li[5]/a")).click();
+        driver.close();
     }
 }
