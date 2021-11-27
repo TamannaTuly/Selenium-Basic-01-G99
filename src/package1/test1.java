c14bb84c2f09f70ff6843921b27d331b32812e0b
TamannaTuly <ttniger53@gmail.com>
2021-11-28 00:36:33 +0600

    Test01-running the driver

--- src/package1/test1.java
+++ src/package1/test1.java
@@ -5,9 +5,26 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class test1 {
     public static void main(String[] args){
-        System.setProperty("Webdriver.chrome.driver","E:\\doctor strange\\chromedriver_win32\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","E:\\doctor strange\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
         String baseUrl = "http://demo.guru99.com/test/newtours/";
+        String expectedTitle = "Welcome: Mercury ToursFailed";
+        String actualTitle = "";
+
+//        running the driver
+        driver.get(baseUrl);
+
+        actualTitle = driver.getTitle();
+
+        if (actualTitle.contentEquals(expectedTitle)){
+            System.out.println(actualTitle);
+            System.out.println("Passed");
+        }
+        else {
+            System.out.println(actualTitle);
+            System.out.println("Failed");
+        }
+        driver.close();
     }
 }
