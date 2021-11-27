6160163397cd102261686968f1bcca86caa19155
TamannaTuly <ttniger53@gmail.com>
2021-11-28 01:07:30 +0600

    Test02-tagName Locator

---
+++ src/package1/test2_0.java
@@ -0,0 +1,28 @@
+package package1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test2_0 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "E:\\doctor strange\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        String baseUrl = "https://www.facebook.com/";
+        String tagName = "";
+        String tagName2 = "";
+
+        driver.get(baseUrl);
+
+        tagName = driver.findElement(By.id("email")).getTagName();
+        tagName2 = driver.findElement(By.id("pass")).getTagName();
+
+        System.out.println(tagName);
+        System.out.println(tagName2);
+        driver.close();
+        System.exit(0);
+
+
+    }
+}
