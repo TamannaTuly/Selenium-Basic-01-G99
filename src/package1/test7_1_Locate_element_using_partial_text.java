158ac45abc7b1ee51504fa8cd44c2bc307b95189
TamannaTuly <ttniger53@gmail.com>
2022-01-23 13:19:41 +0600

    TestCase : Locate Elements by Link Text-Partial Link Text [partia text]

--- src/package1/test7_1_Locate_element_using_partial_text.java
+++ src/package1/test7_1_Locate_element_using_partial_text.java
@@ -4,21 +4,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class test7_Locate_Elements_by_Link_Text_Partial_Link_Text {
-
+public class test7_1_Locate_element_using_partial_text {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "E:\\doctor strange\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
-        String baseURL= "http://demo.guru99.com/test/link.html";
-
-//        full link textd
+        String baseURL = "http://demo.guru99.com/test/link.html";
 
         driver.get(baseURL);
-        driver.findElement(By.linkText("click here")).click();
+        driver.findElement(By.partialLinkText("here")).click();
         System.out.println("Title is : "+ driver.getTitle());
         driver.quit();
-
-
     }
 }
